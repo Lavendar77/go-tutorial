873ecd961a960e46bddd08e08346dc019397aadf
Adeyinka Adefolurin <folurinyinka@gmail.com>
2021-10-19 16:57:17 +0100

    hello world

--- main.go
+++ main.go
@@ -3,14 +3,5 @@ package main
 import "fmt"
 
 func main() {
-	names := [3]int{22, 24, 25}
-
-	updateName(names)
-
-	fmt.Println(names)
-}
-
-func updateName(names [3]int) {
-	names[1] = 50
-	fmt.Println(names)
+	fmt.Println("Hello, world!")
 }
